1ef7b6a649c6bbd7575811ece718b657704ce941
Jacob Goodwin <jacobgoodwin2017@gmail.com>
2025-07-07 00:45:32 -0400

    SoundGroup source file

---
+++ soundgroup.cpp
@@ -0,0 +1,7 @@
+#include "soundgroup.h"
+
+SoundGroup::SoundGroup(HotkeyManager &hkManager, const std::string &name)
+    : hotkeyManager(hkManager), name(name) {}
+
+// TODO: Implement the destructor. Need to deregister hotkeys once they are
+// implemented.

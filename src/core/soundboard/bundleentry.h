9dc3ab3422091b16622ea58ab124510c556eabdc
Jacob Goodwin <jacobgoodwin2017@gmail.com>
2025-08-15 15:26:47 -0400

    Refactor rotateEntries method to ContainerEntry

--- src/core/soundboard/bundleentry.h
+++ src/core/soundboard/bundleentry.h
@@ -18,25 +18,16 @@ public:
   void removeChild(size_t index) override;
   void removeChild(PlayableEntry* entry) override;
 
-  /*! \brief Rotates entries within the bundle.
-   *
-   * This function moves entries [firstIndex, middleIndex) to lastIndex and
-   * [middleIndex, lastIndex) to firstIndex.]
-   * \param firstIndex The starting index of the range to slide.
-   * \param middleIndex The index at which the entries will be split.
-   * \param lastIndex The index where the first part will be moved to.
-   */
-  void rotateEntries(size_t firstIndex, size_t middleIndex, size_t lastIndex);
+  void rotateEntries(size_t firstIndex, size_t middleIndex,
+                     size_t lastIndex) override;
 
   const std::vector<PlayableEntry*> getEntries() const { return children; }
   bool isRandomPlay() const { return randomPlay; }
-  unsigned int getCategory() const { return category; }
   unsigned int getWeightSum() const { return weightSum; }
   unsigned int getOrderTracker() const { return orderTracker; }
   void setRandomPlay(bool random) { randomPlay = random; }
 
 private:
-  unsigned int category = 0;
   unsigned int weightSum = 0;
   unsigned int orderTracker = 0;
   bool randomPlay = true;
--- src/core/soundboard/containerentry.h
+++ src/core/soundboard/containerentry.h
@@ -14,6 +14,18 @@ public:
   virtual void setChildWeight(size_t index, unsigned int weight) = 0;
   virtual void removeChild(size_t index) = 0;
   virtual void removeChild(PlayableEntry* entry) = 0;
+
+  /*! \brief Rotates entries within the bundle.
+   *
+   * This function moves entries [firstIndex, middleIndex) to lastIndex and
+   * [middleIndex, lastIndex) to firstIndex.]
+   * \param firstIndex The starting index of the range to slide.
+   * \param middleIndex The index at which the entries will be split.
+   * \param lastIndex The index where the first part will be moved to.
+   */
+  virtual void rotateEntries(size_t firstIndex, size_t middleIndex,
+                             size_t lastIndex) = 0;
+
   const std::vector<PlayableEntry*>& getChildren() const { return children; }
 
 protected:
